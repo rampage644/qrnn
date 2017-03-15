d6a544809bbeca2369b16cf3072372ecf9f9a6d4
Sergei Turukin <rampage644@gmail.com>
2017-03-15 15:52:04 +0300

    Fix embeddings loading.
    Whole embedding matrix wasn't loaded.

--- util/util.py
+++ util/util.py
@@ -12,32 +12,11 @@ def load_embeddings(path, size, dimensions):
     size = os.stat(path).st_size
     with open(path, 'rb') as ifile:
         pos = 0
-        idx = 1
+        idx = 0
         while pos < size:
             chunk = np.load(ifile)
-            ret[idx:idx+chunk.shape[0], :] = chunk
+            chunk_size = chunk.shape[0]
+            ret[idx:idx+chunk_size, :] = chunk
+            idx += chunk_size
             pos = ifile.tell()
     return ret
-
-
-#%%
-# path = 'data/embeddings.npy'
-
-# r = load_embeddings(path)
-# r[23]
-# #%%
-
-# dictpath = 'data/dict.pckl'
-# with open(dictpath, 'rb') as dfile:
-#     wdict = pickle.load(dfile)
-
-# v1 = r[wdict['king']]
-# v2 = r[wdict['queen']]
-# v3 = r[wdict['man']]
-# v4 = r[wdict['woman']]
-
-
-# r1 = v1 - v3
-# r2 = v2 - v4
-
-# (r2 - r1)
